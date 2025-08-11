93cf4ee9038a74e8de3303214bb8c9988b860c04
Ridan77 <dan.ribak17@gmail.com>
2025-08-11 17:45:34 +0300

    starting edit and details

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -1,22 +1,28 @@
-import { ToyPreview } from "./ToyPreview.jsx"
+import { ToyPreview } from "./ToyPreview.jsx";
+import { Link } from "react-router-dom";
 
 export function ToyList({ toys, onRemoveToy, onEditToy, addToCart }) {
-    
-    return (
-        <ul className="toy-list">
-            {toys.map(toy =>
-                <li className="toy-preview" key={toy._id}>
-                    <ToyPreview toy={toy} />
+  return (
+    <ul className="toy-list">
+      {toys.map((toy) => (
+        <li className="toy-preview" key={toy._id}>
+          <ToyPreview toy={toy} />
 
-                    <div>
-                        <button onClick={() => onRemoveToy(toy._id)}>x</button>
-                        <button onClick={() => onEditToy(toy)}>Edit</button>
-                    </div>
-
-                    <button className="buy" onClick={() => addToCart(toy)}>
+          <div>
+            <button onClick={() => onRemoveToy(toy._id)}>x</button>
+            {/* <button onClick={() => onEditToy(toy)}>Edit</button> */}
+          <button>
+            <Link to={`/toy/edit/${toy._id}`}>Edit</Link>
+          </button>
+          <button>
+            <Link to={`/toy/${toy._id}`}>Details</Link>
+          </button>
+          </div>
+          {/* <button className="buy" onClick={() => addToCart(toy)}>
                         Add to Cart
-                    </button>
-                </li>)}
-        </ul>
-    )
+                    </button> */}
+        </li>
+      ))}
+    </ul>
+  );
 }
--- src/cmps/ToyPreview.jsx
+++ src/cmps/ToyPreview.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
 
 export function ToyPreview({ toy }) {
-
-    return (
-        <article>
-            <h4>{toy.name}</h4>
-            <img src={toy.imgUrl} alt="" />
-            <p>Price: <span>${toy.price.toLocaleString()}</span></p>
-            Labels: <pre> {JSON.stringify(toy.labels)}</pre>
-            <p className={toy.inStock ? 'in-stock' : 'not-instock'} >{toy.inStock ? 'In Stock' : 'Not In Stock'}</p>
-            {toy.owner && <p>Owner: <Link to={`/user/${toy.owner._id}`}>{toy.owner.fullname}</Link></p>}
-            <hr />
-            <Link to={`/toy/edit/${toy._id}`}>Edit</Link> &nbsp; | &nbsp;
-            <Link to={`/toy/${toy._id}`}>Details</Link>
-
-        </article>
-    )
+  return (
+    <article>
+      <h4>{toy.name}</h4>
+      <img src={toy.imgUrl} alt="" />
+      <p>
+        Price: <span>${toy.price.toLocaleString()}</span>
+      </p>
+      Labels: <pre> {JSON.stringify(toy.labels)}</pre>
+      <p className={toy.inStock ? "in-stock" : "not-instock"}>
+        {toy.inStock ? "In Stock" : "Not In Stock"}
+      </p>
+      {toy.owner && (
+        <p>
+          Owner: <Link to={`/user/${toy.owner._id}`}>{toy.owner.fullname}</Link>
+        </p>
+      )}
+      <hr />
+     
+    </article>
+  );
 }
