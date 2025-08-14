bd6d71a96c0e51ad7c5ae0801bad4e058fc1db03
Ridan77 <dan.ribak17@gmail.com>
2025-08-14 17:10:20 +0300

    finish google map

---
+++ src/cmps/ToyMap.jsx
@@ -0,0 +1,105 @@
+import {
+  AdvancedMarker,
+  APIProvider,
+  InfoWindow,
+  Map,
+  Pin,
+  useAdvancedMarkerRef,
+  useMap,
+} from "@vis.gl/react-google-maps";
+import { useState, useEffect } from "react";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const branches = [
+  {
+    city: "Tel Aviv",
+    id: 102,
+    position: { lat: 32.071035, lng: 34.779118 },
+  },
+
+  {
+    city: "Hadera",
+    id: 103,
+    position: { lat: 32.435594930999166, lng: 34.92083787918091 },
+  },
+  {
+    city: "Hulon",
+    id: 101,
+    position: { lat: 32.01840842432221, lng: 34.77865648344314 },
+  },
+];
+
+export function ToyMap() {
+  return (
+    <section className="google-map">
+      <h3>Our Branches:</h3>
+
+      <APIProvider apiKey={API_KEY}>
+        <MapController />
+      </APIProvider>
+    </section>
+  );
+}
+
+function MapController() {
+  const [selectedBranch, setSelectedBranch] = useState(null);
+  const [coords, setCoors] = useState({ lat: 32.0853, lng: 34.7818 });
+
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const map = useMap();
+
+  useEffect(() => {
+    if (map && selectedBranch) {
+      map.panTo(selectedBranch.position);
+      map.setZoom(11);
+    }
+  }, [map, selectedBranch]);
+
+  function onMapClick(ev) {
+    setCoors(ev.detail.latLng);
+    ev.map.panTo(ev.detail.latLng);
+  }
+
+  function handleBranchClick(branch) {
+    setCoors(branch.position);
+    setSelectedBranch(branch);
+  }
+
+  return (
+    <>
+      <button onClick={() => handleBranchClick(branches[0])}>Tel Aviv</button>
+      <button onClick={() => handleBranchClick(branches[1])}>Hadera</button>
+      <button onClick={() => handleBranchClick(branches[2])}>Hulon</button>
+
+      <Map
+        style={{ height: "55vh", width: "100%" }}
+        defaultZoom={11}
+        mapId={"mapid-122223"}
+        defaultCenter={coords}
+        // center={coords}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+        onClick={onMapClick}>
+        {branches.map((branch) => (
+          <AdvancedMarker position={branch.position} key={branch.id}>
+            <Pin />
+          </AdvancedMarker>
+        ))}
+        {selectedBranch && (
+          <InfoWindow anchor={marker} maxWidth={200}>
+            <p>Our branch in {selectedBranch.city}</p>
+          </InfoWindow>
+        )}
+        <AdvancedMarker
+          position={coords}
+          ref={markerRef}
+          title={"AdvancedMarker with customized pin."}>
+          <Pin
+            background={"#22ccff"}
+            borderColor={"#1e89a1"}
+            glyphColor={"#0f677a"}
+          />
+        </AdvancedMarker>
+      </Map>
+    </>
+  );
+}
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,54 +1,14 @@
-import {
-  AdvancedMarker,
-  APIProvider,
-  InfoWindow,
-  Map,
-  Pin,
-  useAdvancedMarkerRef,
-} from "@vis.gl/react-google-maps";
-import { useState } from "react";
-const API_KEY = import.meta.env.VITE_API_KEY;
-console.log(API_KEY);
 
+
+import { ToyMap } from "../cmps/ToyMap";
 
 export function AboutUs() {
-  const [coords, setCoors] = useState({ lat: 32.0853, lng: 34.7818 });
-  const [markerRef, marker] = useAdvancedMarkerRef();
-    function onMapClick(ev) {
-        // console.log('ev:', ev)
-        setCoors(ev.detail.latLng)
-        ev.map.panTo(ev.detail.latLng)
-    }
+
   return (
     <section>
       <h2>About Us</h2>
-      <section className="google-map">
-        <h1>Google Maps!</h1>
-        <APIProvider apiKey={API_KEY}>
-          <Map
-            defaultZoom={11}
-            mapId={"mapid-122223"}
-            defaultCenter={coords}
-            gestureHandling={"greedy"}
-            disableDefaultUI={true}
-            onClick={onMapClick}>
-            <InfoWindow anchor={marker} maxWidth={200}>
-              <p>
-                This is the content for another infowindow with <em>HTML</em>
-                -elements.
-              </p>
-            </InfoWindow>
-            {/* <AdvancedMarker
-              position={coords}
-              ref={markerRef}
-              title={"AdvancedMarker with customized pin."}>
-              <Pin
-                background={"#22ccff"}
-                borderColor={"#1e89a1"}
-                glyphColor={"#0f677a"}></Pin>
-            </AdvancedMarker> */}
-          </Map>
-        </APIProvider>
+      <section >
+        <ToyMap/>
       </section>
     </section>
   );
